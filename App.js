c69a3e6017c01e0371235803b4648316887be087
viictorst <victorsteixeira435@gmail.com>
2023-09-27 20:13:17 -0300

    Subindo arquivos

--- App.js
+++ App.js
@@ -3,10 +3,10 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 
-import Login from "./screens/Login";
-import CreateUser from "./screens/CreateUser"
-import FeedBooks from "./screens/FeedBooks"
-import DetailBook from "./screens/DetailBook";
+import Login from "./src/screens/Login";
+import CreateUser from "./src/screens/CreateUser"
+import FeedBooks from "./src/screens/FeedBooks"
+import DetailBook from "./src/screens/DetailBook";
 
 const Stack = createNativeStackNavigator();
 
